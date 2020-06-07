808d3a85a82609041898390d2a655e351abf0292
zivvers <ziv@uchicago.edu>
2020-06-06 22:39:42 -0500

    minor modifications to BandAlbum class & docker-compose.yml

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # albumBuyerCrawler
+
+`sudo docker-compose build`
+`sudo docker-compose up -d`
+`sudo docker exec -it $(sudo docker ps --filter NAME=albumbuyercrawler_worker -aq) /bin/bash`
--- dir/src/main/scala/com/doctype/BandAlbum.scala
+++ dir/src/main/scala/com/doctype/BandAlbum.scala
@@ -5,7 +5,7 @@ import org.mongodb.scala.bson.ObjectId;
 case class BandAlbum(_id: ObjectId, artist: String, album: String, url: String, location: String)
 
 object BandAlbum {
-   def apply( artist: String, album: String, url: String, location: String): Band =
+   def apply( artist: String, album: String, url: String, location: String): BandAlbum =
       new BandAlbum( new ObjectId(), artist, album, url, location )   
 }
 
